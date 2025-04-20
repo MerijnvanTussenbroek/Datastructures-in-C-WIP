898dc9e7eba21bf66df4a662983e5cf669a9fc8c
Merijn van Tussenbroek <m.j.w.vantussenbroek@students.uu.nl>
2025-04-20 12:48:47 +0200

    finished completely implementing the list datastructure, you can add items, remove items add at a specific index, and even sort it if you make your own comparsino function

--- include/list.h
+++ include/list.h
@@ -10,14 +10,20 @@ typedef struct                          \
     int capacity;                       \
 } name##_list;                          \
                                         \
+typedef struct                          \
+{                                       \
+    int success;                        \
+    type value;                         \
+} name##_result;                        \
+                                        \
 /* datastructure initialization */      \
 name##_list* name##_listCreate(int newSize)                                         \
 {                                                                                   \
     name##_list* list;                                                              \
     list = malloc(sizeof(name##_list));                                             \
-    list->data = malloc((sizeof(type) * newSize) + (4 * sizeof(type)));             \
+    list->data = malloc(sizeof(type) * (newSize + 4));                              \
     list->size = newSize;                                                           \
-    list->capacity = (4 * sizeof(type)) + (sizeof(type) * newSize);                 \
+    list->capacity = (newSize + 4);                                                 \
     return list;                        \
 }                                       \
                                         \
@@ -26,8 +32,8 @@ void name##_addToList(name##_list *l, type value)\
 {                                       \
     if(l->size == l->capacity)          \
     {                                   \
-        type* temp = realloc(l->data, l->capacity * 2);\
-        l->capacity *= 2;               \
+        type* temp = realloc(l->data, sizeof(type) * (l->capacity + 2));\
+        l->capacity += 2;               \
         if(!temp) exit(1);              \
         l->data = temp;                 \
     }                                   \
@@ -36,7 +42,7 @@ void name##_addToList(name##_list *l, type value)\
                                         \
 void name##_changeInList(name##_list *l, type value, int index)\
 {                                       \
-    if(l->size < index){                \
+    if(l->size < index  || index < 0){  \
         printf("Tried changeing an item that doesn't exist.");\
         return;                         \
     }                                   \
@@ -45,33 +51,67 @@ void name##_changeInList(name##_list *l, type value, int index)\
                                         \
 void name##_removeFromList(name##_list *l, int index)\
 {                                       \
-    if(l->size < index){                \
+    if(l->size < index  || index < 0){  \
         printf("Tried deleting an item that doesn't exist.");\
         return;                         \
     }                                   \
     for(int i = index; i < (l->size - 1); i++)\
     {                                   \
-        l->data[index] = l->data[index+1];\
+        l->data[i] = l->data[i+1];\
     }                                   \
     l->size--;                          \
 }                                       \
                                         \
+void name##_insertIntoListAt(name##_list *l, int index, type value)\
+{                                       \
+    if(l->size < index  || index < 0){  \
+        printf("Tried inserting an item at an index that deosn't exit.");\
+        return;                         \
+    }                                   \
+                                        \
+    for(int i = index; i < l->size; i++){\
+        l->data[i+1] = l->data[i];      \
+    }                                   \
+    l->size++;                          \
+    l->data[index] = value;             \
+}                                       \
+                                        \
 void name##_deleteList(name##_list *l)  \
 {                                       \
-    printf("Begin deleting list\n");      \
-    if(!l) return;                      \
-    printf("Pointer is valid, begin deleting\n");    \
-    if(!l->data) {printf("l->data is corrupt or null"); return;} \
+    if(!l) exit(-1);                    \
+    if(!l->data) exit(-1);              \
     free(l->data);                      \
-    printf("Deleted data, deleting list itself\n"); \
     free(l);                            \
 }                                       \
                                         \
-type name##_retrieveFromList(name##_list *l, int index)\
+name##_result name##_retrieveFromList(name##_list *l, int index)\
 {                                       \
-    if(l->size < index)                 \
+    name##_result result =  { 0 };      \
+    if(l->size < index || index < 0)    \
     {                                   \
-        printf("Tried retrieving an itme that doesn't exist.");\
+        printf("Tried retrieving an item that doesn't exist. %d \n", index);\
+        return result;                  \
     }                                   \
-    return l->data[index];              \
-}
+    result.success = 1;                 \
+    result.value = l->data[index];      \
+    return result;                      \
+}                                       \
+/* This only works if the type can be compared */\
+/* To do this, youll have to define your own comparison function that must adhere to the following requirements */\
+/* The output is an integer, if the first input is less than the second, return 1, otherwise return 0*/\
+/*so for example, with 5 < 6 you return 1, but for 8 < 2 you return 0 or anything BUT 1*/\
+void name##_sortList(name##_list *l, int (*cmp)(const type *, const type *))    \
+{                                       \
+    for(int i = 0; i < l->size; i++)    \
+    {                                   \
+        for(int j = i; j < l->size; j++)\
+        {                               \
+            if(cmp(&l->data[i], &l->data[j]) != 1)\
+            {                           \
+                type temp = l->data[i]; \
+                l->data[i] = l->data[j];\
+                l->data[j] = temp;      \
+            }                           \
+        }                               \
+    }                                   \
+}                                        
--- src/testingGrounds.c
+++ src/testingGrounds.c
@@ -2,33 +2,10 @@
 
 DEFINE_LIST(int, int);
 
+
 int main()
 {
-    printf("Begin initializing testing grounds\n");
-    int_list* list = int_listCreate(5);
-
-    printf("Initialized datastructure, adding in data\n");
-
-    int_changeInList(list, 5, 0);
-    int_changeInList(list, 5, 1);
-    int_changeInList(list, 5, 2);
-    int_changeInList(list, 5, 3);
-    int_changeInList(list, 5, 4);
-
-    int_addToList(list, 6);
-    int_addToList(list, 6);
-    int_addToList(list, 6);
-    int_addToList(list, 6);
-    int_addToList(list, 6);
-    int_addToList(list, 6);
-
-    printf("Begin trying to retrieve data from datastructure\n");
-
-    int x = int_retrieveFromList(list, 8);
-    int y = int_retrieveFromList(list, 10);
-    printf("%d %d \n", x, y);
-
-    int_deleteList(list);
+    
 
     printf("Program finished with no problems");
     return 0;
